51af049dbfd19a987805178af009744c2330187b
Bockya <rhk3562@naver.com>
2020-12-22 10:55:21 +0900

    Print value2

--- src/day02/Value.java
+++ src/day02/Value.java
@@ -3,6 +3,7 @@ package day02;
 public class Value {
     public static void main(String[] args) {
         int value = 10;
-        System.out.println(value);
+        int value2 = value + 50;
+        System.out.println(value2);
     }
 }
