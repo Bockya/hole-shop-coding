289b4f80e77f11fa47de906672842ba9cccd3cfc
Bockya <rhk3562@naver.com>
2020-12-30 01:58:18 +0900

    Test : 인스턴스 데이터 안정성 보장 확인

--- src/day06/PigSave.java
+++ src/day06/PigSave.java
@@ -23,7 +23,13 @@ class PigSaveTest {
         PigSave save = new PigSave();
         save.deposit(100);
         save.deposit(500);
-        int totalMoney = save.getTotal(); //get()를 통한 접근
+        int totalMoney = save.getTotal(); //total값을 복사해서 totalMoney에 저장
         System.out.println("총액 : " + totalMoney);
+
+        //강제로 변경
+        totalMoney = 10000; //저장된 데이터만 수정되고 원래 데이터는 안전
+        //PigSave도 변했을까?
+        int afterMoney = save.getTotal();
+        System.out.println("변경되었나? " + afterMoney);
     }
 }
