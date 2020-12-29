e2c5fd733eca1b690c669cb61e11b6fb664a22f8
Bockya <rhk3562@naver.com>
2020-12-30 01:19:21 +0900

    Feat : 메소드의 파라미터 결정

--- src/day06/BMIMachine.java
+++ src/day06/BMIMachine.java
@@ -1,12 +1,14 @@
 package day06;
 
 class BMIMachine {
-    void calculate() {
+    void calculate(float h, float w) {
         System.out.println("비만 지수를 구합니다.");
     }
 
     public static void main(String[] args) {
         BMIMachine m = new BMIMachine();
-        m.calculate();
+        float height = 1.8F;
+        float weight = 77F;
+        m.calculate(height,weight);
     }
 }
