85363555fe1f0b76f0f070c6f8e6d925ac714955
Bockya <rhk3562@naver.com>
2020-12-30 02:33:20 +0900

    Feat : ready() 구현

--- src/day06/BokBulBok.java
+++ src/day06/BokBulBok.java
@@ -1,6 +1,21 @@
 package day06;
 
 public class BokBulBok {
-    void ready(){}
-    void selectOne(){}
+    private char[] items;
+
+    void ready() {
+        items = new char[10];
+        //복불복 당첨 인덱스 번호
+        int index = (int) (Math.random() * items.length);
+        for (int i = 0; i < items.length; i++) {
+            if (i == index){
+                items[i] = 'X';
+                continue;
+            }
+            items[i] = 'O';
+        }
+    }
+
+    void selectOne() {
+    }
 }
