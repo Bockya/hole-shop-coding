a711b8f5522578ba88e7347b31eebb302838d855
Bockya <rhk3562@naver.com>
2020-12-30 01:37:29 +0900

    Feat : 돼지저금통 간단하게 구조 설계

---
+++ src/day06/PigSave.java
@@ -0,0 +1,11 @@
+package day06;
+
+class PigSave {
+    int total;
+    void deposit(){
+        System.out.println("저금통 입금");
+    }
+    void withdraw(){
+        System.out.println("저금통 배따기");
+    }
+}
