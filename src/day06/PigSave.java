8453717bfdf85005cf497fe0f4cbeabb9345fbfb
Bockya <rhk3562@naver.com>
2020-12-30 01:47:59 +0900

    Fix : deposit()에 토탈 값 추가

--- src/day06/PigSave.java
+++ src/day06/PigSave.java
@@ -3,8 +3,9 @@ package day06;
 class PigSave {
     int total;
 
-    void deposit() {
+    void deposit(int amount) {
         System.out.println("저금통 입금");
+        total += amount;
     }
 
     void withdraw() {
@@ -15,9 +16,7 @@ class PigSave {
 class PigSaveTest {
     public static void main(String[] args) {
         PigSave save = new PigSave();
-        save.deposit();
-        save = null;
-        save.deposit();
-        System.out.println(save);
+        save.deposit(100);
+        save.deposit(500);
     }
 }
