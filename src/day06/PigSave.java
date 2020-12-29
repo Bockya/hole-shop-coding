19790860db3572a0f821cdbc84817e173b6f1e61
Bockya <rhk3562@naver.com>
2020-12-30 01:39:45 +0900

    Feat : PigSaveTest 클래스 안에 main() 만들기

--- src/day06/PigSave.java
+++ src/day06/PigSave.java
@@ -2,10 +2,19 @@ package day06;
 
 class PigSave {
     int total;
-    void deposit(){
+
+    void deposit() {
         System.out.println("저금통 입금");
     }
-    void withdraw(){
+
+    void withdraw() {
         System.out.println("저금통 배따기");
+    }
+}
+
+class PigSaveTest {
+    public static void main(String[] args) {
+        PigSave save = new PigSave();
+        System.out.println(save);
     }
 }
