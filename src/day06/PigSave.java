6a3541d58b060f412a7e2f271fe96ad54d831146
Bockya <rhk3562@naver.com>
2020-12-30 01:55:29 +0900

    Add : getTotal(); 변수 데이터 복사

--- src/day06/PigSave.java
+++ src/day06/PigSave.java
@@ -1,7 +1,11 @@
 package day06;
 
 class PigSave {
-    int total;
+    private int total; //캡슐화
+
+    int getTotal() { //진짜 total값 x, 데이터 복사(copy)
+        return total;
+    }
 
     void deposit(int amount) {
         System.out.println("저금통 입금");
@@ -10,6 +14,7 @@ class PigSave {
 
     void withdraw() {
         System.out.println("저금통 배따기");
+        total = 0;
     }
 }
 
@@ -18,5 +23,7 @@ class PigSaveTest {
         PigSave save = new PigSave();
         save.deposit(100);
         save.deposit(500);
+        int totalMoney = save.getTotal(); //get()를 통한 접근
+        System.out.println("총액 : " + totalMoney);
     }
 }
