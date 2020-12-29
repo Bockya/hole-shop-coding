5c7c3c6cc7ba026630de21ff80a5f57a16373230
Bockya <rhk3562@naver.com>
2020-12-30 02:39:14 +0900

    Feat : main() 구현

--- src/day06/BokBulBok.java
+++ src/day06/BokBulBok.java
@@ -9,7 +9,7 @@ public class BokBulBok {
         //복불복 당첨 인덱스 번호
         int index = (int) (Math.random() * items.length);
         for (int i = 0; i < items.length; i++) {
-            if (i == index){
+            if (i == index) {
                 items[i] = 'X';
                 continue;
             }
@@ -19,5 +19,20 @@ public class BokBulBok {
 
     char selectOne() {
         return items[nextIndex++];
+    }
+}
+
+class BokBulBokTest {
+    public static void main(String[] args) {
+        BokBulBok bok = new BokBulBok();
+
+        //복불복 기계 안에 준비 작업
+        bok.ready();
+
+        //결과 10번 중에 하나는 x
+        for (int i = 0; i < 10; i++) {
+            char result = bok.selectOne();
+            System.out.println(i + "번째: " + result);
+        }
     }
 }
