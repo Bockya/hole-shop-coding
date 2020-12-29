d11eca3a16ffc538455d0f5e6d20325a50945528
Bockya <rhk3562@naver.com>
2020-12-30 01:16:17 +0900

    Feat : BMIMachine 클래스 선언, calculate() 메서드 결정

---
+++ src/day06/BMIMachine.java
@@ -0,0 +1,12 @@
+package day06;
+
+class BMIMachine {
+    void calculate() {
+        System.out.println("비만 지수를 구합니다.");
+    }
+
+    public static void main(String[] args) {
+        BMIMachine m = new BMIMachine();
+        m.calculate();
+    }
+}
