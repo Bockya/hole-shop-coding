0bd162a0506fd913e2a36335566a09c868d2592e
Bockya <rhk3562@naver.com>
2020-12-31 11:49:36 +0900

    Feat : calculate 메서드 return 값 만들기

--- src/day08/org/bmi/service/BMIService.java
+++ src/day08/org/bmi/service/BMIService.java
@@ -2,7 +2,6 @@ package day08.org.bmi.service;
 
 class BMIService {
     double calculate(double height, double weight){
-        double result = 0;
-        return result;
+        return weight / (height * height);
     }
 }
