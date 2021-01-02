042be11cf18f5ce4074a2d1fc35fa3bffe3e0b6d
Bockya <rhk3562@naver.com>
2021-01-02 17:10:17 +0900

    Add getResult(); 비만도 결과

--- src/day06/BMITest.java
+++ src/day06/BMITest.java
@@ -1,6 +1,31 @@
 package day06;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 import java.util.Scanner;
+
+@AllArgsConstructor
+@ToString
+class PersonData {
+    String name;
+    double height;
+    double weight;
+
+    double calc() {
+        return this.weight / (this.height * this.height);
+    }
+
+    String getResult(double indexValue) {
+        if (indexValue > 25)
+            return "비만";
+        else if (indexValue > 23)
+            return "과체중";
+        else if (indexValue > 18.5)
+            return "정상";
+        else return "저체중";
+    }
+}
 
 class BMITest {
     public static void main(String[] args) {
@@ -20,6 +45,7 @@ class BMITest {
         PersonData pd = new PersonData(name, height, weight);
         System.out.println(pd);
         double bmi = pd.calc();
-        System.out.println(bmi);
+        System.out.println("비만지수 : " + bmi);
+        System.out.println("건강지수 : " + pd.getResult(bmi));
     }
 }
