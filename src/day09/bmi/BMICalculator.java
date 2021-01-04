d4d4e9167d0dad0a70ac16a3769b420f934246d6
Bockya <rhk3562@naver.com>
2021-01-04 14:14:56 +0900

    Feat : bmi

---
+++ src/day09/bmi/BMICalculator.java
@@ -0,0 +1,10 @@
+package day09.bmi;
+
+class BMICalculator {
+    double calc(PersonData personData){
+        double result = 0;
+        double height = personData.getHeight();
+        double weight = personData.getWeight();
+        return result;
+    }
+}
