f5890b24a43e5517b3a68dbcbe72054b27899916
류희경 <rhk3562@naver.com>
2021-01-24 12:26:42 +0900

    생성자 추가

--- src/Employee/Employee.java
+++ src/Employee/Employee.java
@@ -1,7 +1,18 @@
 package Employee;
 
 class Employee {
-    private String sabun;
-    private String name;
-    private String ssn;
+    private String sabun; // 사번
+    private String name; // 이름
+    private String ssn; // 주민번호
+
+    // 생성자
+    public Employee(String sabun, String name) {
+        this(sabun, name, "unknows");
+    }
+
+    public Employee(String sabun, String name, String ssn) {
+        this.sabun = sabun;
+        this.name = name;
+        this.ssn = ssn;
+    }
 }
