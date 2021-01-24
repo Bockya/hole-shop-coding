bb7d85905bdd7555147d682164ae30b148f4b988
류희경 <rhk3562@naver.com>
2021-01-24 12:34:43 +0900

    파라미터로 데이터 객체를 이용한 add 메서드 동작 확인

--- src/Employee/EmployeeService.java
+++ src/Employee/EmployeeService.java
@@ -1,4 +1,16 @@
 package Employee;
 
 class EmployeeService {
+    void add(Employee emp) {
+        System.out.println("신규 인사정보를 추가하는 기능 테스트");
+        System.out.println(emp.getName());
+        System.out.println(emp.getSabun());
+        System.out.println(emp.getSsn());
+    }
+
+    public static void main(String[] args) {
+        EmployeeService service = new EmployeeService();
+        Employee newEmp = new Employee("123", "홍길동", "111-222");
+        service.add(newEmp);
+    }
 }
