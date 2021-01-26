b91d55d724b8cf9bb32489285a9f67b7cfabd199
류희경 <rhk3562@naver.com>
2021-01-27 01:02:07 +0900

    Add : 추상클래스 선언

---
+++ src/abstractEmployee/Employee.java
@@ -0,0 +1,8 @@
+package abstractEmployee;
+
+public abstract class Employee {
+    protected String empno;
+    protected String name;
+    protected int pay;
+    public abstract double getMonthPay();
+}
