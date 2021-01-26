557c500d7195c2314181b0d574ae2323393b4591
류희경 <rhk3562@naver.com>
2021-01-27 01:12:45 +0900

    Add : bonus Fix : constructor

--- src/abstractEmployee/RegularEmployee.java
+++ src/abstractEmployee/RegularEmployee.java
@@ -1,8 +1,10 @@
 package abstractEmployee;
 
 public class RegularEmployee extends Employee {
+    private int bonus;
     public RegularEmployee(String empno, String name, int pay, int bonus) {
-        super(empno, name, pay);
+        super(empno, name, pay); // 코드 절약
+        this.bonus = bonus; // 필요한 데이터 추가
     }
 
     public double getMonthPay() {
