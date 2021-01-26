8bc962d93ce97f955796c023897622792520c6dd
류희경 <rhk3562@naver.com>
2021-01-27 01:03:15 +0900

    Add : toString()

--- src/abstractEmployee/Employee.java
+++ src/abstractEmployee/Employee.java
@@ -4,5 +4,15 @@ public abstract class Employee {
     protected String empno;
     protected String name;
     protected int pay;
+
     public abstract double getMonthPay();
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empno='" + empno + '\'' +
+                ", name='" + name + '\'' +
+                ", pay=" + pay +
+                '}';
+    }
 }
