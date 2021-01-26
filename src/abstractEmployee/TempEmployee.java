6caf73aa611fdc5ec98fddeb23f97266e735a201
류희경 <rhk3562@naver.com>
2021-01-27 01:19:47 +0900

    Add : hireYear 변수

--- src/abstractEmployee/TempEmployee.java
+++ src/abstractEmployee/TempEmployee.java
@@ -1,9 +1,13 @@
 package abstractEmployee;
 
 public class TempEmployee extends Employee {
+    // 1. 변수 선언
+    private int hireYear;
+
     // 2. 생성자 속 super 생성
     public TempEmployee(String empno, String name, int pay, int hireYear) {
         super(empno, name, pay);
+        this.hireYear = hireYear;
     }
 
     // 1. 추상클래스 상속받으니깐 오버라이딩부터!
