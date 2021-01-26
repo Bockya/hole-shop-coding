6150f901d7c71df49e615fed8aef21295fcf82ee
류희경 <rhk3562@naver.com>
2021-01-27 01:18:17 +0900

    Feat : 생성자{super()}

--- src/abstractEmployee/TempEmployee.java
+++ src/abstractEmployee/TempEmployee.java
@@ -1,8 +1,13 @@
 package abstractEmployee;
 
-public class TempEmployee extends Employee{
+public class TempEmployee extends Employee {
+    // 2. 생성자 속 super 생성
+    public TempEmployee(String empno, String name, int pay, int hireYear) {
+        super(empno, name, pay);
+    }
+
     // 1. 추상클래스 상속받으니깐 오버라이딩부터!
-    public double getMonthPay(){
+    public double getMonthPay() {
         return 0;
     }
 }
