c3628c8cc152e11150235ee1b7dfd523e95bd1a1
류희경 <rhk3562@naver.com>
2021-01-27 01:14:02 +0900

    Fix : getMonthPay()

--- src/abstractEmployee/RegularEmployee.java
+++ src/abstractEmployee/RegularEmployee.java
@@ -2,12 +2,13 @@ package abstractEmployee;
 
 public class RegularEmployee extends Employee {
     private int bonus;
+
     public RegularEmployee(String empno, String name, int pay, int bonus) {
         super(empno, name, pay); // 코드 절약
         this.bonus = bonus; // 필요한 데이터 추가
     }
 
     public double getMonthPay() {
-        return 0;
+        return (pay / (double) 12) + (bonus / (double) 12);
     }
 }
